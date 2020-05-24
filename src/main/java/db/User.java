2c722e6f3a136f85610040cec665872f9015e276
stefangeorgievuzunov <stefan.georgiev.uzunov@gmail.com>
2020-05-24 19:14:33 +0300

    directory structure created.

--- src/main/java/db/User.java
+++ src/main/java/db/User.java
@@ -1,4 +1,15 @@
 package db;
 
+import enums.Gender;
+
 public class User {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private Gender gender;
+    private Integer age;
+    private String description;
+    private String password;
+
+
 }
