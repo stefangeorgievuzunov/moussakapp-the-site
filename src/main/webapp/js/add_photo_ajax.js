70970c8d89e68f743044566bbad8e92142c23901
stefangeorgievuzunov <stefan.georgiev.uzunov@gmail.com>
2020-06-24 23:55:52 +0300

    Image's uploading functionality is almost done(95%). File's size and type is validated correctly.

--- src/main/webapp/js/add_photo_ajax.js
+++ src/main/webapp/js/add_photo_ajax.js
@@ -1,37 +1,34 @@
 $(document).ready(function () {
 
-    function uploadImg(input){
-        if (input.files && input.files[0]){
-            let reader=new FileReader();
+    function uploadImg(input) {
+        if (input.files && input.files[0]) {
+            let reader = new FileReader();
 
             let formData = new FormData($("#upload")[0]);
             formData.append('uploadedFile', input.files[0]);
 
-            $("#save").prop("disabled", true);
+            let saveButton = $("#save");
+            saveButton.prop("disabled", true);
 
             let message = $("#message");
             message.addClass("text-danger");
+            message.hide();
 
-            reader.onload=function (e) {
-                console.log("E TARGET RESULT: "+e.target.result);
-               $("#image").attr('src',e.target.result);
+            reader.onload = function (e) {
+                $("#image").attr('src', e.target.result);
             }
 
             $.ajax({
                 url: '/admin/new/recipe/upload/image',
-                enctype: 'multipart/form-data',
                 type: 'post',
                 data: formData,
                 processData: false,
                 contentType: false,
                 success: function (data) {
                     if (data.success) {
-                        console.log(data);
-                        console.log("input.files"+input.files[0]);
-                        console.log("formdata file: "+formData.get('uploadedFile'));
 
                         reader.readAsDataURL(input.files[0]);
-
+                        saveButton.prop("disabled", false);
                     } else {
                         message.html(data.error).show();
                     }
